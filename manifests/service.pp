ff548101656bd405a49837910c0a4d59d88cce79
Salimane Adjao Moustapha <salimane.moustapha@meltwater.com>
2015-09-04 15:30:36 +0200

    Disable the gor service automatic configuration

--- manifests/service.pp
+++ manifests/service.pp
@@ -5,17 +5,19 @@
 class gor::service {
   $service_ensure = $::gor::service_ensure
   $service_enable = $service_ensure ? {
-    stopped => false,
-    false   => false,
+    stopped => true,
+    false   => true,
+    absent  => false,
     default => true,
   }
 
   # Disable `hasrestart` because upstart's `initctl restart` doesn't reload
   # the config to pick up the new `exec` command when our arguments change.
-  service { 'gor':
-    ensure     => $service_ensure,
-    enable     => $service_enable,
-    hasstatus  => true,
-    hasrestart => false,
-  }
+  # service { 'gor':
+  #   ensure     => $service_ensure,
+  #   enable     => $service_enable,
+  #   hasstatus  => true,
+  #   hasrestart => false,
+  #   provider   => 'upstart',
+  # }
 }
