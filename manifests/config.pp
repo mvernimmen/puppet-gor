c73705916eebcd278abe7b04fb8a42da09f1a515
Salimane Adjao Moustapha <salimane.moustapha@meltwater.com>
2015-09-04 15:34:43 +0200

    add log file to the gor upstart script

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,9 +3,17 @@
 # Private class. Should not be called directly.
 #
 class gor::config {
-  $args = $::gor::args
+  $ensure = $::gor::ensure
+  $args   = $::gor::args
+
+  file { '/var/log/gor':
+    ensure => directory,
+    mode   => '0755',
+  }
 
   file { '/etc/init/gor.conf':
+    ensure  => $ensure,
     content => template('gor/etc/init/gor.conf.erb'),
+    require => File['/var/log/gor']
   }
 }
