5b1de7f02b3626ceb279a91cb77e5f88bb6031d5
Salimane Adjao Moustapha <salimane.moustapha@meltwater.com>
2015-10-13 13:09:38 +0200

    disable service on empty args and add override on stooped

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,21 +3,37 @@
 # Private class. Should not be called directly.
 #
 class gor::service {
-  $service_ensure = $::gor::service_ensure
-  $service_enable = $service_ensure ? {
-    stopped => true,
-    false   => true,
-    absent  => false,
-    default => true,
+  $args           = $::gor::args
+  
+  if empty($args) {
+    $override_ensure = absent
+    $service_enable = false
+  } else {
+    $ensure = present
+    $service_ensure = $::gor::service_ensure
+    $service_enable = $service_ensure ? {
+      stopped => false,
+      false   => false,
+      default => true,
+    }
   }
 
-  # Disable `hasrestart` because upstart's `initctl restart` doesn't reload
-  # the config to pick up the new `exec` command when our arguments change.
-  # service { 'gor':
-  #   ensure     => $service_ensure,
-  #   enable     => $service_enable,
-  #   hasstatus  => true,
-  #   hasrestart => false,
-  #   provider   => 'upstart',
-  # }
+  if !$service_enable {
+    file { '/etc/init/gor.override':
+      ensure  => $override_ensure,
+      content => "start on manual\nstop on manual"
+    }
+  } else {
+    # don't try to stop the service if it was started through capistrano
+    # or manually
+    # Disable `hasrestart` because upstart's `initctl restart` doesn't reload
+    # the config to pick up the new `exec` command when our arguments change.
+    service { 'gor':
+      ensure     => $service_ensure,
+      enable     => $service_enable,
+      hasstatus  => true,
+      hasrestart => false,
+      provider   => 'upstart',
+    }
+  }
 }
