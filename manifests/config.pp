1d50e1d7171c380e5f35bd3220ace717fd6a44a0
Max Vernimmen <m.vernimmen@comparegroup.eu>
2016-09-27 09:21:18 +0200

    Sort documentation by abc, update version and add runuser for possible security improvement

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,8 +10,9 @@ class gor::config {
     $ensure = $::gor::ensure
   }
 
-  $envvars     = $::gor::envvars
   $binary_path = $::gor::binary_path
+  $envvars     = $::gor::envvars
+  $gor_runuser = $::gor::runuser
 
   $log_ensure = $ensure ? {
     present => directory,
--- manifests/init.pp
+++ manifests/init.pp
@@ -4,35 +4,40 @@
 #
 # === Parameters
 #
+# [*args*]
+#   Hash of arguments to run Gor with. Values will be single quoted.
+#
+# [*binary_path*]
+#   Specify the location of the Gor binary.
+#
 # [*ensure*]
 #   Ensure parameter to pass to the package.
 #   Default: present
-#
-# [*version*]
-#   version of the package.
-#   Default: 0.14.1
-#
-# [*service_ensure*]
-#   Ensure parameter to pass to the service.
-#   Default: running
 #
 # [*envvars*]
 #   Environment variables to be passed into the upstart config. This could
 #   be used to turn on debugging options.
 #
-# [*binary_path*]
-#   Specify the location of the Gor binary.
+# [*runuser*]
+#   Defines as which user gor executable should be running.
 #
-# [*args*]
-#   Hash of arguments to run Gor with. Values will be single quoted.
+# [*service_ensure*]
+#   Ensure parameter to pass to the service.
+#   Default: running
+#
+# [*version*]
+#   version of the package.
+#   Default: 0.15.1
+#
 #
 class gor (
   $ensure         = present,
-  $version        = '0.14.1',
+  $version        = '0.15.1',
   $digest_string  = 'ced467f51da7491a227b871c9894d351',
   $digest_type    = 'md5',
   $source_url     = undef,
   $manage_service = true,
+  $runuser        = 'root',
   $service_ensure = running,
   $envvars        = {},
   $binary_path    = '/usr/local/bin/gor',
@@ -54,3 +59,4 @@ class gor (
   Class['gor::package'] ~> Class['gor::config']
   Class['gor::config'] ~> Class['gor::service']
 }
+
